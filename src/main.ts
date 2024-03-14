d57716de292933915e598818fdbfad232006421e
ncbinh98 <42426543+ncbinh98@users.noreply.github.com>
2024-03-14 13:58:41 +0700

    Setup Fastify (#5)
    Co-authored-by: BM Bean <bean@brickmate.vn>

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { connectionSource } from './config/typeorm';
 import { VersioningType } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
@@ -14,6 +22,6 @@ async function bootstrap() {
   });
   await connectionSource.initialize();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
