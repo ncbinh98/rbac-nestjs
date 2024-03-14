d58302e1008cd1466557b306f7086a93c7bb85fc
BM Bean <bean@brickmate.vn>
2024-03-14 10:57:43 +0700

    Enable versioning api

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { connectionSource } from './config/typeorm';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1',
+  });
   await connectionSource.initialize();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
